2fb32702ad45df5d80cb5fcdf0c6df80c1f765bf
Vinh B <vinh.bui0101@gmail.com>
2025-01-19 15:32:52 -0800

    go back

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  assetPrefix: '/valentines/',
 };
 
 export default nextConfig;

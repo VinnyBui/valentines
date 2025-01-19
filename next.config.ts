9ddffccb2354aeaf61856f355bdc5162a6cfd971
Vinh B <vinh.bui0101@gmail.com>
2025-01-19 15:11:57 -0800

    d

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  output: 'export',
+  assetPrefix: './', // Ensure assets are loaded relative to the site root
 };
 
 export default nextConfig;
